b5f982035b99f7000a32fb1d75a63c258a16beef
schwe <bs103@hdm-stuttgart.de>
2022-02-17 02:49:06 +0100

    ++

--- lib/render_sliver_known_extents_list.dart
+++ lib/render_sliver_known_extents_list.dart
@@ -186,7 +186,7 @@ abstract class RenderSliverKnownExtentsBoxAdaptor
     final double targetEndScrollOffset = scrollOffset + remainingExtent;
 
     BoxConstraints childConstraints(int index) {
-      if (index < 0 || index > itemHeights.length - 1) {
+      if (index < 0 || index >= itemHeights.length - 1) {
         return constraints.asBoxConstraints(
           minExtent: 0.0,
           maxExtent: 0.0,
